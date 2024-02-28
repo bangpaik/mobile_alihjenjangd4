b1e60cac89151eaf5e8d8206b10351155b21a4f6
Rizki Pm <udarizki2020@gmail.com>
2024-02-28 20:57:26 +0700

    update get data api

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_styled_toast/flutter_styled_toast.dart';
 import 'package:mobile_alihjenjangd4/screen_page/page_bottombar.dart';
 import 'package:mobile_alihjenjangd4/screen_page/page_kedua.dart';
 import 'package:mobile_alihjenjangd4/screen_page/page_ketiga.dart';
+import 'package:mobile_alihjenjangd4/screen_page/page_list_berita.dart';
 import 'package:mobile_alihjenjangd4/screen_page/page_list_user.dart';
 
 void main() {
@@ -61,121 +62,141 @@ class PageUtama extends StatelessWidget {
 
         //child : hanya bisa menampung 1 widget didalamnya
         //children : bisa menampung lebih dari 1 widget (column, row, list, stack)
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset('gambar/logo_pnp.png',height: 150, width: 150,),
-            Image.asset('gambar/movie/film9.png',height: 150, width: 150,),
-            Text('Alih Jenjang D4 PNP'),
-            SizedBox(
-              height: 10,
-            ),
-            Text('Teknik Informatika'),
-            SizedBox(
-              height: 20,
-            ),
-            Text('Politeknik Negeri Padang'),
-            SizedBox(
-              height: 30,
-            ),
-            MaterialButton(
-              onPressed: () {
-                showToast('This is normal toast with animation',
-                    context: context,
-                    animation: StyledToastAnimation.slideFromRight,
-                    reverseAnimation: StyledToastAnimation.slideToRight,
-                    position: StyledToastPosition.top,
-                    startOffset: Offset(1.0, 0.0),
-                    reverseEndOffset: Offset(1.0, 0.0),
-                    animDuration: Duration(seconds: 1),
-                    duration: Duration(seconds: 4),
-                    curve: Curves.linearToEaseOut,
-                    reverseCurve: Curves.fastOutSlowIn);
-              },
-              child: Text('Tekan Ini'),
-              color: Colors.green,
-              textColor: Colors.white,
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            MaterialButton(
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                  content: Text('ini adalah snackbar'),
-                  backgroundColor: Colors.deepOrangeAccent,
-                  action: SnackBarAction(
-                    label: "OK",
-                    onPressed: () {},
-                  ),
-                ));
-              },
-              color: Colors.green,
-              textColor: Colors.white,
-              child: Text('Snackbar'),
-            ),
+        child: Padding(
+          padding: EdgeInsets.all(7),
+          
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset('gambar/logo_pnp.png',height: 150, width: 150,),
+                Image.asset('gambar/movie/film9.png',height: 150, width: 150,),
+                Text('Alih Jenjang D4 PNP'),
+                SizedBox(
+                  height: 10,
+                ),
+                Text('Teknik Informatika'),
+                SizedBox(
+                  height: 20,
+                ),
+                Text('Politeknik Negeri Padang'),
+                SizedBox(
+                  height: 30,
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    showToast('This is normal toast with animation',
+                        context: context,
+                        animation: StyledToastAnimation.slideFromRight,
+                        reverseAnimation: StyledToastAnimation.slideToRight,
+                        position: StyledToastPosition.top,
+                        startOffset: Offset(1.0, 0.0),
+                        reverseEndOffset: Offset(1.0, 0.0),
+                        animDuration: Duration(seconds: 1),
+                        duration: Duration(seconds: 4),
+                        curve: Curves.linearToEaseOut,
+                        reverseCurve: Curves.fastOutSlowIn);
+                  },
+                  child: Text('Tekan Ini'),
+                  color: Colors.green,
+                  textColor: Colors.white,
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text('ini adalah snackbar'),
+                      backgroundColor: Colors.deepOrangeAccent,
+                      action: SnackBarAction(
+                        label: "OK",
+                        onPressed: () {},
+                      ),
+                    ));
+                  },
+                  color: Colors.green,
+                  textColor: Colors.white,
+                  child: Text('Snackbar'),
+                ),
+            
+                SizedBox(height: 10,),
+                MaterialButton(onPressed: (){
+                  //pindah ke page lain
+                  //navigator push : dari class A ke class B
+                  //navigator pop : class B ke class A
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>
+                    PageNavigation()
+                  ));
+            
+                },
+                  child: Text('Page Navigation'),
+                  color: Colors.deepOrangeAccent,
+                  textColor: Colors.white,
+                ),
+                SizedBox(height: 10,),
+                MaterialButton(onPressed: (){
+                  //pindah ke page lain
+                  //navigator push : dari class A ke class B
+                  //navigator pop : class B ke class A
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>
+                      PagePassingData()
+                  ));
+            
+                },
+                  child: Text('Page Passing Data'),
+                  color: Colors.deepOrangeAccent,
+                  textColor: Colors.white,
+                ),
+            
+                SizedBox(height: 10,),
+                MaterialButton(onPressed: (){
+                  //pindah ke page lain
+                  //navigator push : dari class A ke class B
+                  //navigator pop : class B ke class A
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>
+                      PageBottomBar()
+                  ));
+            
+                },
+                  child: Text('Bottom Menu'),
+                  color: Colors.deepOrangeAccent,
+                  textColor: Colors.white,
+                ),
+                SizedBox(height: 10,),
+                MaterialButton(onPressed: (){
+                  //pindah ke page lain
+                  //navigator push : dari class A ke class B
+                  //navigator pop : class B ke class A
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>
+                      PageListUser()
+                  ));
+            
+                },
+                  child: Text('List Data User'),
+                  color: Colors.deepOrangeAccent,
+                  textColor: Colors.white,
+                ),
+                SizedBox(height: 10,),
+                MaterialButton(onPressed: (){
+                  //pindah ke page lain
+                  //navigator push : dari class A ke class B
+                  //navigator pop : class B ke class A
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>
+                      PageListBerita()
+                  ));
 
-            SizedBox(height: 10,),
-            MaterialButton(onPressed: (){
-              //pindah ke page lain
-              //navigator push : dari class A ke class B
-              //navigator pop : class B ke class A
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>
-                PageNavigation()
-              ));
-
-            },
-              child: Text('Page Navigation'),
-              color: Colors.deepOrangeAccent,
-              textColor: Colors.white,
+                },
+                  child: Text('List Berita'),
+                  color: Colors.deepOrangeAccent,
+                  textColor: Colors.white,
+                ),
+            
+            
+              ],
             ),
-            SizedBox(height: 10,),
-            MaterialButton(onPressed: (){
-              //pindah ke page lain
-              //navigator push : dari class A ke class B
-              //navigator pop : class B ke class A
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>
-                  PagePassingData()
-              ));
-
-            },
-              child: Text('Page Passing Data'),
-              color: Colors.deepOrangeAccent,
-              textColor: Colors.white,
-            ),
-
-            SizedBox(height: 10,),
-            MaterialButton(onPressed: (){
-              //pindah ke page lain
-              //navigator push : dari class A ke class B
-              //navigator pop : class B ke class A
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>
-                  PageBottomBar()
-              ));
-
-            },
-              child: Text('Bottom Menu'),
-              color: Colors.deepOrangeAccent,
-              textColor: Colors.white,
-            ),
-            SizedBox(height: 10,),
-            MaterialButton(onPressed: (){
-              //pindah ke page lain
-              //navigator push : dari class A ke class B
-              //navigator pop : class B ke class A
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>
-                  PageListUser()
-              ));
-
-            },
-              child: Text('List Data User'),
-              color: Colors.deepOrangeAccent,
-              textColor: Colors.white,
-            ),
-
-
-          ],
+          ),
         ),
       ),
     );
---
+++ lib/screen_page/page_list_berita.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mobile_alihjenjangd4/models/model_berita.dart';
+
+class PageListBerita extends StatefulWidget {
+  const PageListBerita({super.key});
+
+  @override
+  State<PageListBerita> createState() => _PageListBeritaState();
+}
+
+class _PageListBeritaState extends State<PageListBerita> {
+
+  Future<List<Datum>?> getBerita() async{
+    try{
+      http.Response res = await http.get(Uri.parse('http://192.168.1.30:8080/beritaDb/getBerita.php'));
+      return modelBeritaFromJson(res.body).data;
+    }catch(e){
+      setState(() {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(e.toString()))
+        );
+      });
+    }
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Berita Nasional'),
+        backgroundColor: Colors.cyan,
+      ),
+      
+      body: FutureBuilder(
+        future: getBerita(),
+        builder: (BuildContext context, AsyncSnapshot<List<Datum>?> snapshot){
+          if(snapshot.hasData){
+            return ListView.builder(
+              itemCount: snapshot.data?.length,
+              itemBuilder: (context, index){
+                Datum? data = snapshot.data?[index];
+                return Padding(
+                  padding: EdgeInsets.all(8),
+                  child: GestureDetector(
+                    onTap: (){},
+                    child: Card(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(8),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network('http://192.168.1.30:8080/beritaDb/gambar_berita/${data?.gambarBerita}',
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            title: Text("${data?.judul}",
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                            subtitle: Text("${data?.isiBerita}",
+                              maxLines: 2,
+                              style: TextStyle(
+                                fontSize: 10,
+                                color: Colors.black54
+                              ),
+                            ),
+
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          }else if (snapshot.hasError){
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          }else{
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.orange,
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
